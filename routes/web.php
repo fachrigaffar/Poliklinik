a590118d8f7288bc39d4f5e6fb1f8a4439aac103
Fachri Gaffar Ilhami <fachrigaffar@gmail.com>
2025-06-04 13:30:46 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -20,33 +20,34 @@ Route::middleware(['auth', 'role:dokter'])->prefix('dokter')->group(function () 
     Route::get('/dashboard', function () {
         return view('dokter.dashboard');
     })->name('dokter.dashboard');
+
+    Route::prefix('obat')->group(function () {
+            Route::get('/', [ObatController::class, 'index'])->name('dokter.obat.index');
+            Route::get('/create', [ObatController::class, 'create'])->name('dokter.obat.create');
+            Route::post('/', [ObatController::class, 'store'])->name('dokter.obat.store');
+            Route::get('/{id}/edit', [ObatController::class, 'edit'])->name('dokter.obat.edit');
+            Route::patch('/{id}', [ObatController::class, 'update'])->name('dokter.obat.update');
+            Route::delete('/{id}', [ObatController::class, 'destroy'])->name('dokter.obat.destroy');
+    });
+
+    Route::prefix('jadwal-periksa')->group(function () {
+        Route::get('/', [JadwalPeriksaController::class, 'index'])->name('dokter.jadwal.index');
+        Route::get('/create', [JadwalPeriksaController::class, 'create'])->name('dokter.jadwal.create');
+        Route::post('/', [JadwalPeriksaController::class, 'store'])->name('dokter.jadwal.store');
+        Route::get('/{id}/edit', [JadwalPeriksaController::class, 'edit'])->name('dokter.jadwal.edit');
+        Route::put('/{id}', [JadwalPeriksaController::class, 'update'])->name('dokter.jadwal.update');
+        Route::delete('/{id}', [JadwalPeriksaController::class, 'destroy'])->name('dokter.jadwal.destroy');
+
+        Route::patch('/{jadwal}/status', [JadwalPeriksaController::class, 'updateStatus'])->name('dokter.jadwal.status');
+    });
 });
-
-Route::prefix('obat')->group(function () {
-        Route::get('/', [ObatController::class, 'index'])->name('dokter.obat.index');
-        Route::get('/create', [ObatController::class, 'create'])->name('dokter.obat.create');
-        Route::post('/', [ObatController::class, 'store'])->name('dokter.obat.store');
-        Route::get('/{id}/edit', [ObatController::class, 'edit'])->name('dokter.obat.edit');
-        Route::patch('/{id}', [ObatController::class, 'update'])->name('dokter.obat.update');
-        Route::delete('/{id}', [ObatController::class, 'destroy'])->name('dokter.obat.destroy');
-});
-
-Route::prefix('jadwal-periksa')->group(function () {
-    Route::get('/', [JadwalPeriksaController::class, 'index'])->name('dokter.jadwal.index');
-    Route::get('/create', [JadwalPeriksaController::class, 'create'])->name('dokter.jadwal.create');
-    Route::post('/', [JadwalPeriksaController::class, 'store'])->name('dokter.jadwal.store');
-    Route::get('/{id}/edit', [JadwalPeriksaController::class, 'edit'])->name('dokter.jadwal.edit');
-    Route::put('/{id}', [JadwalPeriksaController::class, 'update'])->name('dokter.jadwal.update');
-    Route::delete('/{id}', [JadwalPeriksaController::class, 'destroy'])->name('dokter.jadwal.destroy');
-
-    Route::patch('/{jadwal}/status', [JadwalPeriksaController::class, 'updateStatus'])->name('dokter.jadwal.status');
-});
-
 
 Route::middleware(['auth', 'role:pasien'])->prefix('pasien')->group(function () {
     Route::get('/dashboard', function () {
         return view('pasien.dashboard');
     })->name('pasien.dashboard');
+
+
 });
 
 require __DIR__.'/auth.php';
